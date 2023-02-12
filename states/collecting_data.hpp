c048b86970c8b10f68cab73c314bdbd66b2df6b2
geekskick <geeks_kick@yahoo.co.uk>
2023-02-12 16:57:44 +0000

    Collecting Data looks nice

--- states/collecting_data.hpp
+++ states/collecting_data.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "template_state.hpp"
+#include <iostream>
 
 class CollectingDataState final : public TemplateState
 {
@@ -8,11 +9,39 @@ class CollectingDataState final : public TemplateState
 
     void template_run(const uint8_t bit, StateMachineInterface &sm) override
     {
+        std::cout << "bit = " << bit << "\tbits received before this = " << bits_received_ << "\n";
+        if (at_word_boundary() && packet_end(bit))
+        {
+            std::cout << "\tPacket rxd\n";
+            sm.transition_to_packet_received();
+        }
+        else if (at_word_boundary() && !packet_end(bit))
+        {
+            // do nothing as we are expecting to see more bits
+            std::cout << "\tAt a word boundary but expecting more data\n";
+            bits_received_ = 0;
+        }
+        else
+        {
+            std::cout << "\tPushing to the buffer\n";
+            sm.push_buffer(bit);
+            bits_received_++;
+        }
     }
 
     void reset_self() override
     {
         bits_received_ = 0;
+    }
+
+    bool packet_end(const uint8_t bit) const
+    {
+        return bit == PACKET_END_BIT;
+    }
+
+    bool at_word_boundary() const
+    {
+        return bits_received_ != 0 && bits_received_ % BITS_IN_WORD == 0;
     }
 
 public:
--- tests/collecting_state_test.cc
+++ tests/collecting_state_test.cc
@@ -1,3 +1,4 @@
+#include "mock_state_machine.hpp"
 #include "states/collecting_data.hpp"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -6,4 +7,56 @@ TEST(CollectingDataStateTest, SignalsOnValidEndOfData)
 {
     auto bf = BitFactory::Builder{}.with_zero_tolerance(BitTolerance{1, 3}).with_one_tolerance(BitTolerance{4, 6}).build();
     auto uut = CollectingDataState{bf};
+    auto sm = testing::StrictMock<MockStateMachine>{};
+
+    EXPECT_CALL(sm, push_buffer(0)).Times(CollectingDataState::BITS_IN_WORD);
+
+    for (int i = 0; i < CollectingDataState::BITS_IN_WORD; i++)
+    {
+        uut.run(Microseconds{1}, sm);
+    }
+
+    EXPECT_CALL(sm, transition_to_packet_received()).Times(1);
+    uut.run(Microseconds{1}, sm);
+}
+
+TEST(CollectingDataStateTest, SignalsOnValidEndOfDataAfterTwoWords)
+{
+    auto bf = BitFactory::Builder{}.with_zero_tolerance(BitTolerance{1, 3}).with_one_tolerance(BitTolerance{4, 6}).build();
+    auto uut = CollectingDataState{bf};
+    auto sm = testing::StrictMock<MockStateMachine>{};
+
+    EXPECT_CALL(sm, push_buffer(0)).Times(CollectingDataState::BITS_IN_WORD * 2);
+
+    for (int i = 0; i < CollectingDataState::BITS_IN_WORD; i++)
+    {
+        uut.run(Microseconds{1}, sm);
+    }
+
+    uut.run(Microseconds{5}, sm); // another word incoming
+
+    for (int i = 0; i < CollectingDataState::BITS_IN_WORD; i++)
+    {
+        uut.run(Microseconds{1}, sm);
+    }
+    EXPECT_CALL(sm, transition_to_packet_received()).Times(1);
+    uut.run(Microseconds{1}, sm);
+}
+
+TEST(CollectingDataStateTest, ResetsOnInvalidBitMidWord)
+{
+    auto bf = BitFactory::Builder{}.with_zero_tolerance(BitTolerance{1, 3}).with_one_tolerance(BitTolerance{4, 6}).build();
+    auto uut = CollectingDataState{bf};
+    auto sm = testing::StrictMock<MockStateMachine>{};
+
+    EXPECT_CALL(sm, push_buffer(0)).Times(CollectingDataState::BITS_IN_WORD - 2);
+
+    for (int i = 0; i < CollectingDataState::BITS_IN_WORD - 2; i++)
+    {
+        uut.run(Microseconds{1}, sm);
+    }
+
+    EXPECT_CALL(sm, reset_buffer()).Times(1);
+    EXPECT_CALL(sm, reset_to_waiting_preamble()).Times(1);
+    uut.run(Microseconds{10}, sm);
 }
